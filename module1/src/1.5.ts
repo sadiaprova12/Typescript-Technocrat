0e185e1ffeb2cb4eef902b8cef8b5a5efc5009e6
sadiaprova12 <sadiaprova51717@gmail.com>
2024-05-08 22:05:31 +0600

    object optional and literal types

---
+++ module1/src/1.5.ts
@@ -0,0 +1,18 @@
+// Reference Type --> Object
+
+
+const user :{
+    company: 'Programming Hero'; //type --> literal types
+    firstName: string;
+    middleName?: string; //optional type
+    lastName: string;
+    isMarriend: boolean;
+}= {
+    company: 'Programming Hero',
+    firstName: 'Sadia',
+    // middleName: 'Siddika',
+    lastName: 'Prova',
+    isMarriend: true,
+}
+
+user.company='Programming Hero'
